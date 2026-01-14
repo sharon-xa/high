c56bbe9c11292f1d5ddef12d0d52896212d0339f
sharon <ali93456@gmail.com>
2026-01-14 05:49:39 +0300

    feat: implement drag-and-drop functionality and block deletion in BlockElement component
    - Added drag-and-drop support for reordering blocks within the editor.
    - Implemented block deletion functionality with a delete button.
    - Enhanced the BlockElement component to manage drag states and visual feedback during interactions.

--- src/components/editor/block/BlockElement.tsx
+++ src/components/editor/block/BlockElement.tsx
@@ -1,5 +1,8 @@
+import { useRef, useState, type DragEvent, type JSX, type KeyboardEvent } from "react";
+import { GripVertical, Trash } from "lucide-react";
+import { useEditorStore } from "../../../stores/editorStores/editorStore";
 import type { Block } from "../../../types/editor/block.types";
-import { type KeyboardEvent } from "react";
+
 import ParagraphBlock from "./ParagraphBlock";
 import HeaderBlock from "./HeaderBlock";
 import CodeBlock from "./CodeBlock";
@@ -14,9 +17,15 @@ type Props = {
 };
 
 const BlockElement = ({ block, index, setRef, keyDownOnBlock }: Props) => {
+	const { blocks, activeBlockIndex, reorderBlocks, deleteBlock } = useEditorStore();
+	const [isDragging, setIsDragging] = useState<boolean>(false);
+	const [isHovered, setIsHovered] = useState<boolean>(false);
+	const dragImageRef = useRef<HTMLDivElement>(null);
+	let blockElement: JSX.Element;
+
 	switch (block.type) {
 		case "paragraph":
-			return (
+			blockElement = (
 				<ParagraphBlock
 					block={block}
 					index={index}
@@ -24,8 +33,9 @@ const BlockElement = ({ block, index, setRef, keyDownOnBlock }: Props) => {
 					keyDownOnBlock={keyDownOnBlock}
 				/>
 			);
+			break;
 		case "header":
-			return (
+			blockElement = (
 				<HeaderBlock
 					block={block}
 					index={index}
@@ -33,8 +43,9 @@ const BlockElement = ({ block, index, setRef, keyDownOnBlock }: Props) => {
 					keyDownOnBlock={keyDownOnBlock}
 				/>
 			);
+			break;
 		case "code":
-			return (
+			blockElement = (
 				<CodeBlock
 					block={block}
 					index={index}
@@ -42,8 +53,9 @@ const BlockElement = ({ block, index, setRef, keyDownOnBlock }: Props) => {
 					keyDownOnBlock={keyDownOnBlock}
 				/>
 			);
+			break;
 		case "image":
-			return (
+			blockElement = (
 				<ImageBlockComponent
 					block={block}
 					index={index}
@@ -51,8 +63,9 @@ const BlockElement = ({ block, index, setRef, keyDownOnBlock }: Props) => {
 					keyDownOnBlock={keyDownOnBlock}
 				/>
 			);
+			break;
 		case "separator":
-			return (
+			blockElement = (
 				<SeparatorBlock
 					block={block}
 					index={index}
@@ -60,9 +73,89 @@ const BlockElement = ({ block, index, setRef, keyDownOnBlock }: Props) => {
 					keyDownOnBlock={keyDownOnBlock}
 				/>
 			);
+			break;
 		default:
 			return null;
 	}
+
+	const handleDragStart = (e: DragEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+
+		e.dataTransfer.effectAllowed = "move";
+		e.dataTransfer.setData("text/plain", index.toString());
+
+		if (dragImageRef.current) {
+			e.dataTransfer.setDragImage(dragImageRef.current, 0, 0);
+		}
+
+		setIsDragging(true);
+	};
+
+	const handleDragEnd = (e: DragEvent<HTMLButtonElement>) => {
+		e.stopPropagation();
+		setIsDragging(false);
+		setIsHovered(false);
+	};
+
+	const handleDragOver = (e: DragEvent<HTMLElement>) => {
+		e.preventDefault();
+		e.stopPropagation();
+		e.dataTransfer.dropEffect = "move";
+		setIsHovered(true);
+	};
+
+	const handleDragLeave = (e: DragEvent<HTMLElement>) => {
+		e.preventDefault();
+		e.stopPropagation();
+		setIsHovered(false);
+	};
+
+	const handleDrop = (e: DragEvent<HTMLElement>) => {
+		e.preventDefault();
+		e.stopPropagation();
+
+		const draggedIndex = parseInt(e.dataTransfer.getData("text/plain"));
+		reorderBlocks(draggedIndex, index);
+
+		setIsDragging(false);
+		setIsHovered(false);
+	};
+
+	const handleBlockDeletion = () => {
+		deleteBlock(index);
+	};
+
+	blockElement = (
+		<div
+			className={`relative py-1 ${isDragging ? "opacity-50" : ""} border-b-2 ${isHovered ? "border-primary" : "border-transparent"}`}
+			onDragOver={handleDragOver}
+			onDragLeave={handleDragLeave}
+			onDrop={handleDrop}
+		>
+			<div
+				className={`absolute absolute-ver-center -left-24 ${index === activeBlockIndex && blocks.length > 1 ? "flex" : "hidden"} items-center justify-center gap-4`}
+			>
+				<button onClick={handleBlockDeletion} className="cursor-pointer active:scale-95">
+					<Trash />
+				</button>
+				<button
+					draggable
+					onDragStart={handleDragStart}
+					onDragEnd={handleDragEnd}
+					className="cursor-grab active:cursor-grabbing"
+				>
+					<GripVertical />
+				</button>
+			</div>
+			{blockElement}
+
+			<div ref={dragImageRef} className="hidden">
+				Dragging block {index}
+			</div>
+		</div>
+	);
+
+	return blockElement;
 };
 
 export default BlockElement;
